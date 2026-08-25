adc7afd63f5f5d24d306d6e4259c05d58a5f02ae
jiabo <843051627@qq.com>
2026-08-26 02:26:20 +0800

    chore: copy profile validation input

--- internal/model/profile.go
+++ internal/model/profile.go
@@ -60,8 +60,9 @@ func (p Profile) Validate() error {
 	if len(p.Steps) < 2 {
 		problems = append(problems, FieldError{Field: "steps", Message: "至少需要两个阶段"})
 	}
-	sequences := make([]int, 0, len(p.Steps))
-	for _, step := range p.Steps {
+	steps := append([]ProfileStep(nil), p.Steps...)
+	sequences := make([]int, 0, len(steps))
+	for _, step := range steps {
 		sequences = append(sequences, step.Sequence)
 		if err := step.Validate(); err != nil {
 			problems = append(problems, FieldError{Field: fmt.Sprintf("step.%d", step.Sequence), Message: err.Error()})
