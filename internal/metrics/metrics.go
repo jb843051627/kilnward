557436aef3b91e5eb1bb89b76c53aa2d56a87696
jiabo <843051627@qq.com>
2026-08-27 04:43:17 +0800

    feat: update kilnward logic

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -9,11 +9,21 @@ type Registry struct {
 
 func New() *Registry { return &Registry{values: map[string]int64{}} }
 
-func (r *Registry) Add(key string, delta int64) { r.values[key] += delta }
+func (r *Registry) Add(key string, delta int64) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.values[key] += delta
+}
 
-func (r *Registry) Get(key string) int64 { return r.values[key] }
+func (r *Registry) Get(key string) int64 {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+	return r.values[key]
+}
 
 func (r *Registry) Snapshot() map[string]int64 {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
 	out := make(map[string]int64, len(r.values))
 	for key, value := range r.values {
 		out[key] = value
