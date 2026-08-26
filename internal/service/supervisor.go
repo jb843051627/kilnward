72c8adef493ed3ed02113f94187b70cf0551e73f
jiabo <843051627@qq.com>
2026-08-27 00:38:19 +0800

    init: quality baseline

---
+++ internal/service/supervisor.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"github.com/jb843051627/kilnward/internal/ingest"
+	"github.com/jb843051627/kilnward/internal/model"
+)
+
+func (a *App) QueueReadingCheck(ctx context.Context, reading model.Reading) (<-chan error, error) {
+	if err := a.requireContext(ctx); err != nil {
+		return nil, err
+	}
+	done := make(chan error, 1)
+	job := ingest.Job{ID: reading.ID, Done: done, Run: func(jobCtx context.Context) error {
+		_, err := a.RecordReading(jobCtx, reading)
+		if err != nil {
+			return fmt.Errorf("reading job %s: %w", reading.ID, err)
+		}
+		return nil
+	}}
+	if err := a.queue.Submit(context.Background(), job); err != nil {
+		return nil, err
+	}
+	return done, nil
+}
+
+func (a *App) DrainReadingCheck(ctx context.Context, done <-chan error) error {
+	select {
+	case err := <-done:
+		return err
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+}
